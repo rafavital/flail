9ef4ae16a86584d146bd9c795ae962566d171175
Rafael <rvitall.alves@gmail.com>
2020-04-18 18:07:33 -0300

    flailing now directs you towards the nearest puddle

--- Assets/scripts/FlailController.cs
+++ Assets/scripts/FlailController.cs
@@ -8,23 +8,31 @@ public class FlailController : MonoBehaviour
     [SerializeField] private float flailForce = 600;
     [SerializeField] private float flailTorque;
     [SerializeField] private string puddleTag;
-
+    [SerializeField] private Vector2 minMaxX;
+    [SerializeField] private Vector2 minMaxY;
+    
+    private Vector2 closestPuddle;
     private Rigidbody2D rb;
     private Rigidbody2D[] bodyParts;
     private Vector2 flailDir;
+    private GameObject[] puddles;
     private bool flail, grounded;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D> ();
         bodyParts = GetComponentsInChildren<Rigidbody2D> ();
+        puddles = GameObject.FindGameObjectsWithTag(puddleTag);
     }
-
     void Update()
     {
         if (Input.GetKeyDown (KeyCode.Space) && grounded) {
-            flailDir = Random.insideUnitCircle;
-            flailDir.y = 1;
+            closestPuddle = GetClosestPuddle ();
+            flailDir = new Vector2 (
+                Mathf.Sign (closestPuddle.x - transform.position.x) * Random.Range (minMaxX.x,minMaxX.y),
+                Random.Range (minMaxY.x, minMaxY.y)
+            );
+            
             flail = true;
         } else {
             flail = false;
@@ -39,6 +47,22 @@ public class FlailController : MonoBehaviour
                 bodyParts[i].AddTorque (Random.Range (-flailTorque, flailTorque));
             }
         }
+    }
+
+    private Vector2 GetClosestPuddle () {
+        Vector2 closestPuddle = Vector2.zero;
+        for (int i = 0; i < puddles.Length; i++)
+        {   
+            var currentPuddle = puddles[i];
+            if (i == 0) {
+                closestPuddle = currentPuddle.transform.position;
+            } else {
+                float currentDistance = Vector2.Distance (transform.position, closestPuddle);
+                float newDistance = Vector2.Distance (transform.position, currentPuddle.transform.position);
+                if (newDistance < currentDistance) closestPuddle = currentPuddle.transform.position;
+            }
+        }
+        return closestPuddle;
     }
 
     private void OnCollisionEnter2D (Collision2D other) {
