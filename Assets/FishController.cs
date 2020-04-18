47c241605da0f19e1a3036966de790afb52efd94
Rafael <rvitall.alves@gmail.com>
2020-04-18 02:41:34 -0300

    fish launch controller

---
+++ Assets/FishController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class FishController : MonoBehaviour
+{
+    [SerializeField] private float launchForce;
+
+    private Rigidbody2D rb;
+    private Vector2 launchDir;
+    private Vector2 initialMousePos;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            initialMousePos = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp (0)) { 
+            launchDir = (Vector2) Input.mousePosition - initialMousePos;
+            
+            rb.AddForce (launchDir * launchForce);
+        }
+    }
+
+    private void FixedUpdate() {
+        
+    }
+}
