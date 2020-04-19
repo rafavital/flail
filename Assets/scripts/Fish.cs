1598488e52ef677fb38abcc1c31ae19919b92862
Rafael <rvitall.alves@gmail.com>
2020-04-18 22:47:59 -0300

    imported ui

--- Assets/scripts/Fish.cs
+++ Assets/scripts/Fish.cs
@@ -7,11 +7,33 @@ public class Fish : MonoBehaviour
     public bool puddle, grounded; 
     public string puddleTag = "Puddle";
     [SerializeField] private int initialDashCount = 1;
+    [SerializeField] private int initialBreath = 100;
+    [SerializeField] private float breathDecreaseRate = 0.1f;
+    
+    private float _breath;
+    public float Breath {
+        get => _breath; 
+        set {
+            _breath = value;
+            _breath = Mathf.Clamp (_breath, 0, initialBreath);
+        }
+    }
     private int _dashs;
+    private bool dying;
+
     public int Dashs {get => _dashs; set => _dashs = value;}
 
     private void Start() {
         Dashs = initialDashCount;
+        Breath = initialBreath;
+    }
+
+    private void Update() {
+        if (!puddle) {
+            Breath -= breathDecreaseRate;
+        }
+
+        if (Breath <= 0) Debug.Log("I'M DEAD, STOP THE GAME!");
     }
 
     public void UseDash () {
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+#region SINGLETON
+    public GameManager Instance;
+    private void Awake() {
+        if (Instance != this) Destroy(Instance);
+        if (Instance == null) Instance = this;
+    }
+#endregion
+
+    public void EndGame () {
+        
+    }
+}
