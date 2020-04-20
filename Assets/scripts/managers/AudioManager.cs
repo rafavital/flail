3ede7a8a01688b46077c86e558a887fca4d84dc3
Rafael <rvitall.alves@gmail.com>
2020-04-20 16:37:49 -0300

    Update AudioManager.cs

--- Assets/scripts/managers/AudioManager.cs
+++ Assets/scripts/managers/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    private AudioSource audioSource
+    private AudioSource audioSource;
     public void PlaySound (AudioClip clip, Vector2 position) {
         audioSource.clip = clip;
         audioSource.transform.position = position;
